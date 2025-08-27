b6b885423e0c3e54f455213a64e8bd1a5dd01651
Rtwo <Rian180403@edu.unipar.br>
2025-08-27 19:58:19 -0300

    att2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,10 @@ import entidades.Servico
 import enumeradores.Habilidades
 import enumeradores.Setor
 import enumeradores.Sexo
+import ui.menu
 
 import java.math.BigDecimal
 
 fun main() {
-    val instalacao : Servico = Servico(
-        preco = "800,50".toBigDecimal(),
-        Funcionario(
-            1234567895,
-            "João",
-            20,
-            Sexo.MASCULINO,
-            "Instalador",
-            BigDecimal("200"),
-            2,
-            Habilidades.ESPERTO,//isso são classes aninhadas
-            Setor.MONTAGEM
-
-        )
-    )
+    menu()
 }
---
+++ src/main/kotlin/ui/Menu.kt
@@ -0,0 +1,8 @@
+package ui
+
+fun menu(){
+    println("1 - cadastrar caida de agua")
+    println("2 - editar caixa de agual")
+    println("3 - Listar caixas de agua")
+    println("4 - Excluir caixa de agua")
+}
